0f9ad9de2eb83f358e0a94d7f6667408d511c6dc
Md Erfan <merfan@crimson.ua.edu>
2025-12-04 16:47:08 -0600

    TCP Server Java GPT Response

--- TCPServer.java
+++ TCPServer.java
@@ -1,53 +1,41 @@
-package filesendtest;
 
 import java.io.*;
 import java.net.*;
 
 class TCPServer {
 
-    private final static String fileToSend = "C:\\test1.pdf";
-
-    public static void main(String args[]) {
-
+    public static void main(String args[]) throws Exception {
+        ServerSocket welcomeSocket = new ServerSocket(3248);
+        System.out.println("Server started!");
+        
         while (true) {
-            ServerSocket welcomeSocket = null;
-            Socket connectionSocket = null;
-            BufferedOutputStream outToClient = null;
-
-            try {
-                welcomeSocket = new ServerSocket(3248);
-                connectionSocket = welcomeSocket.accept();
-                outToClient = new BufferedOutputStream(connectionSocket.getOutputStream());
-            } catch (IOException ex) {
-                // Do exception handling
-            }
-
-            if (outToClient != null) {
-                File myFile = new File( fileToSend );
+            Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("New client connected: " + connectionSocket.getInetAddress().getHostAddress());
+            
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            
+            String clientSentence = inFromClient.readLine();
+            String capitalizedSentence = "";
+            
+            if (clientSentence.equals("download")) {
+                File myFile = new File("C:\\Users\\cguo\\11.lsp");
                 byte[] mybytearray = new byte[(int) myFile.length()];
-
-                FileInputStream fis = null;
-
-                try {
-                    fis = new FileInputStream(myFile);
-                } catch (FileNotFoundException ex) {
-                    // Do exception handling
-                }
+                FileInputStream fis = new FileInputStream(myFile);
                 BufferedInputStream bis = new BufferedInputStream(fis);
-
-                try {
-                    bis.read(mybytearray, 0, mybytearray.length);
-                    outToClient.write(mybytearray, 0, mybytearray.length);
-                    outToClient.flush();
-                    outToClient.close();
-                    connectionSocket.close();
-
-                    // File sent, exit the main method
-                    return;
-                } catch (IOException ex) {
-                    // Do exception handling
-                }
+                bis.read(mybytearray, 0, mybytearray.length);
+                OutputStream os = connectionSocket.getOutputStream();
+                System.out.println("Sending file to client...");
+                os.write(mybytearray, 0, mybytearray.length);
+                os.flush();
+            } else {
+                capitalizedSentence = clientSentence.toUpperCase() + "\n";
+                outToClient.writeBytes(capitalizedSentence);
             }
+            
+            outToClient.close();
+            inFromClient.close();
+            connectionSocket.close();
         }
     }
 }
